7e0e816825e138497aeecdffc020e1f68008c8d2
Henna <57915266+hnnav@users.noreply.github.com>
2023-09-28 16:30:46 +0300

    Switch languages

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,31 @@ import Blog from './blog';
 
 function App() {
   const [blogs, setBlogs] = useState([])
+  const [language, setLanguage] = useState('en-US')
 
   useEffect(() => {
     async function fetchData() {
-      const response = await client.getEntries()
+      const response = await client.getEntries({locale: language})
       const entries = response.items.map(item => item.fields)
       setBlogs(entries)
     }
     fetchData()
-  }, [])
+  }, [language])
 
-  console.log(blogs)
-  
+  const handleSwitchlanguage = () => {
+    language === 'en-US' ? setLanguage('fi-FI') : setLanguage('en-US')
+  }
+
   return (
     <>
       <h1>Blog posts</h1>
+      <button onClick={handleSwitchlanguage}>Switch language</button>
 
-      {blogs.map(blog => (
+      {blogs.map((blog, index) => (
         <Blog 
-          title={blog.title} 
-          content={blog.content} 
+          key={index}
+          title={blog.title}
+          content={blog.content}
           img={blog.image.fields.file.url}
           author={blog.author}
         />
