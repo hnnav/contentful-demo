3e6b48a463441bef2e1e9c61b32ad5f0b6d93d63
Henna <57915266+hnnav@users.noreply.github.com>
2023-10-02 10:19:04 +0300

    Add images

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -4,7 +4,7 @@ function Blog({title, content, img, author}) {
   return(
     <div className='w-3/4 max-w-md font-body flex flex-col justify-center gap-3 bg-white p-5 rounded-md'>
       <h3 className='font-title text-xl'>{title}</h3>
-      {/* <img src=''/> */}
+      <img src={img} className='max-h-96 object-cover'/>
       <p>{content}</p>
       <p className='text-xs'>{author}</p>
     </div>
